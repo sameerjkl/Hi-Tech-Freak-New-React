dabbde5146ec7e1bce95b3a1dbbbae28e0f3a8b6
sameerjkl <66202514+sameerjkl@users.noreply.github.com>
2023-09-23 18:07:29 +0530

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -72,10 +72,7 @@ const Home = (props) => {
             <span className="home-text04">From nothing to something</span>
           </h1>
           <span className="home-text05">
-            <span className="home-text06">
-              The highest status people in human history are those that asked
-              for nothing and gave everything
-            </span>
+            Turning dreams into reality, one step at a time
           </span>
         </div>
         <div className="home-container06">
@@ -85,14 +82,14 @@ const Home = (props) => {
               src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/laptop.jpg"
               className="home-image01"
             />
-            <span className="home-text07 Small">
-              <span className="home-text08">
+            <span className="home-text06 Small">
+              <span className="home-text07">
                 &quot;Over the span of the satellite record, Arctic sea ice has
                 been declining significantly, while sea ice in the Antarctichas
                 increased very slightly&quot;
               </span>
               <br></br>
-              <span className="home-text10">-NOAA</span>
+              <span className="home-text09">-NOAA</span>
             </span>
             <div className="home-container08">
               <img
@@ -109,46 +106,42 @@ const Home = (props) => {
               className="home-image03"
             />
             <div className="home-container10">
-              <h3 className="HeadingTwo">
-                <span className="home-text12">
-                  So what does the new record for the lowest level of winter ice
-                  actually mean
-                </span>
+              <h3 className="home-text10">
+                Hi-Tech Freak&apos;s mission is to be at the forefront of
+                technological advancements, leading the way in delivering
+                innovative solutions that drive growth, efficiency, and positive
+                change in businesses and the lives of individuals.
               </h3>
               <p>
-                <br></br>
-                <span className="home-text14">
-                  The Arctic Ocean freezes every winter and much of the sea-ice
-                  then thaws every summer, and that process will continue
-                  whatever happens with climate change. Even if the Arctic
-                  continues to be one of the fastest-warming regions of the
-                  world, it will always be plunged into bitterly cold polar dark
-                  every winter. And year-by-year, for all kinds of natural
-                  reasons, there’s huge variety of the state of the ice.
+                <span>
+                  Hi-Tech Freak is not just a company; it&apos;s a mindset
+                  dedicated to pushing the boundaries of what technology can
+                  achieve. We live and breathe innovation, with a passionate
+                  commitment to staying at the forefront of the tech industry.
+                  Our team of tech enthusiasts combines expertise with a
+                  relentless pursuit of cutting-edge solutions, ensuring that
+                  our clients always benefit from the latest advancements. At
+                  Hi-Tech Freak, we don&apos;t merely follow trends; we set
+                  them. Our mission is to transform businesses and elevate the
+                  human experience through the power of technology. Join us in
+                  embracing the future where technology meets passion, and
+                  together, we&apos;ll redefine what&apos;s possible in the
+                  world of tech.
                 </span>
                 <br></br>
-                <span></span>
                 <br></br>
-                <span className="home-text17">
-                  For a start, it does not automatically follow that a record
-                  amount of ice will melt this summer. More important for
-                  determining the size of the annual thaw is the state of the
-                  weather as the midnight sun approaches and temperatures rise.
-                  But over the more than 30 years of satellite records,
-                  scientists have observed a clear pattern of decline,
-                  decade-by-decade.
-                </span>
-                <br></br>
-                <span></span>
-                <br></br>
-                <span className="home-text20">
-                  The Arctic Ocean freezes every winter and much of the sea-ice
-                  then thaws every summer, and that process will continue
-                  whatever happens with climate change. Even if the Arctic
-                  continues to be one of the fastest-warming regions of the
-                  world, it will always be plunged into bitterly cold polar dark
-                  every winter. And year-by-year, for all kinds of natural
-                  reasons, there’s huge variety of the state of the ice.
+                <span>
+                  At Hi-Tech Freak, our dedication to technology is not just
+                  about the latest gadgets and trends; it&apos;s about the
+                  meaningful impact we can make on the world. We believe in
+                  harnessing the potential of technology to solve real-world
+                  problems, drive positive change, and create a brighter future.
+                  Whether it&apos;s through our innovative products, services,
+                  or consulting, we aim to empower businesses and individuals to
+                  thrive in an increasingly digital world. With a firm
+                  commitment to excellence and a genuine passion for what we do,
+                  Hi-Tech Freak is your trusted partner on the journey to
+                  technological excellence and transformation.
                 </span>
                 <br></br>
               </p>
@@ -160,14 +153,14 @@ const Home = (props) => {
         <div className="home-container11">
           <div className="home-container12">
             <div className="home-container13">
-              <h2 className="home-text21 HeadingOne">Work with us</h2>
-              <p className="home-text22 Lead">
-                <span className="home-text23">
+              <h2 className="home-text17 HeadingOne">Work with us</h2>
+              <p className="home-text18 Lead">
+                <span className="home-text19">
                   Whatever your qualification is - we got you!
                 </span>
               </p>
-              <p className="home-text24 Body">
-                <span className="home-text25">
+              <p className="home-text20 Body">
+                <span className="home-text21">
                   &quot;Take up one idea. Make that one idea your life - think
                   of it, dream of it, live on that idea. Let the brain, muscles,
                   nerves, every part of your body, be full of that idea, and
@@ -176,8 +169,8 @@ const Home = (props) => {
                   world.&quot;
                 </span>
               </p>
-              <p className="home-text26">Mark Jojansen</p>
-              <p className="home-text27 Small">Project manager, Atlassian</p>
+              <p className="home-text22">Mark Jojansen</p>
+              <p className="home-text23 Small">Project manager, Atlassian</p>
               <div className="home-container14">
                 <img
                   alt="image"
@@ -277,15 +270,15 @@ const Home = (props) => {
               <path d="M363.722 722.052l41.298-57.816-45.254-45.256-57.818 41.296c-10.722-5.994-22.204-10.774-34.266-14.192l-11.682-70.084h-64l-11.68 70.086c-12.062 3.418-23.544 8.198-34.266 14.192l-57.818-41.298-45.256 45.256 41.298 57.816c-5.994 10.72-10.774 22.206-14.192 34.266l-70.086 11.682v64l70.086 11.682c3.418 12.060 8.198 23.544 14.192 34.266l-41.298 57.816 45.254 45.256 57.818-41.296c10.722 5.994 22.204 10.774 34.266 14.192l11.682 70.084h64l11.68-70.086c12.062-3.418 23.544-8.198 34.266-14.192l57.818 41.296 45.254-45.256-41.298-57.816c5.994-10.72 10.774-22.206 14.192-34.266l70.088-11.68v-64l-70.086-11.682c-3.418-12.060-8.198-23.544-14.192-34.266zM224 864c-35.348 0-64-28.654-64-64s28.652-64 64-64 64 28.654 64 64-28.652 64-64 64zM1024 384v-64l-67.382-12.25c-1.242-8.046-2.832-15.978-4.724-23.79l57.558-37.1-24.492-59.128-66.944 14.468c-4.214-6.91-8.726-13.62-13.492-20.13l39.006-56.342-45.256-45.254-56.342 39.006c-6.512-4.766-13.22-9.276-20.13-13.494l14.468-66.944-59.128-24.494-37.1 57.558c-7.812-1.892-15.744-3.482-23.79-4.724l-12.252-67.382h-64l-12.252 67.382c-8.046 1.242-15.976 2.832-23.79 4.724l-37.098-57.558-59.128 24.492 14.468 66.944c-6.91 4.216-13.62 8.728-20.13 13.494l-56.342-39.006-45.254 45.254 39.006 56.342c-4.766 6.51-9.278 13.22-13.494 20.13l-66.944-14.468-24.492 59.128 57.558 37.1c-1.892 7.812-3.482 15.742-4.724 23.79l-67.384 12.252v64l67.382 12.25c1.242 8.046 2.832 15.978 4.724 23.79l-57.558 37.1 24.492 59.128 66.944-14.468c4.216 6.91 8.728 13.618 13.494 20.13l-39.006 56.342 45.254 45.256 56.342-39.006c6.51 4.766 13.22 9.276 20.13 13.492l-14.468 66.944 59.128 24.492 37.102-57.558c7.81 1.892 15.742 3.482 23.788 4.724l12.252 67.384h64l12.252-67.382c8.044-1.242 15.976-2.832 23.79-4.724l37.1 57.558 59.128-24.492-14.468-66.944c6.91-4.216 13.62-8.726 20.13-13.492l56.342 39.006 45.256-45.256-39.006-56.342c4.766-6.512 9.276-13.22 13.492-20.13l66.944 14.468 24.492-59.13-57.558-37.1c1.892-7.812 3.482-15.742 4.724-23.79l67.382-12.25zM672 491.2c-76.878 0-139.2-62.322-139.2-139.2s62.32-139.2 139.2-139.2 139.2 62.322 139.2 139.2c0 76.878-62.32 139.2-139.2 139.2z"></path>
             </svg>
           </div>
-          <h2 className="home-text28 HeadingTwo">
-            <span className="home-text29">Beware The Components</span>
+          <h2 className="home-text24 HeadingTwo">
+            <span className="home-text25">Beware The Components</span>
           </h2>
-          <h3 className="home-text30 HeadingTwo">
-            <span className="home-text31">How To Handle Them</span>
+          <h3 className="home-text26 HeadingTwo">
+            <span className="home-text27">How To Handle Them</span>
             <br></br>
           </h3>
-          <span className="home-text32">
-            <span className="home-text33">
+          <span className="home-text28">
+            <span className="home-text29">
               We’re constantly trying to express ourselves and actualize our
               dreams. Don&apos;t stop.
             </span>
@@ -298,8 +291,8 @@ const Home = (props) => {
               <svg viewBox="0 0 987.4285714285713 1024" className="home-icon2">
                 <path d="M438.857 508.571l312 312c-79.429 80.571-190.286 130.286-312 130.286-242.286 0-438.857-196.571-438.857-438.857s196.571-438.857 438.857-438.857v435.429zM545.714 512h441.714c0 121.714-49.714 232.571-130.286 312zM950.857 438.857h-438.857v-438.857c242.286 0 438.857 196.571 438.857 438.857z"></path>
               </svg>
-              <h1 className="home-text34 HeadingOne">Search and Discover!</h1>
-              <span className="home-text35">
+              <h1 className="home-text30 HeadingOne">Search and Discover!</h1>
+              <span className="home-text31">
                 <span>Website visitors</span>
                 <span></span>
               </span>
@@ -340,8 +333,8 @@ const Home = (props) => {
               <svg viewBox="0 0 1152 1024" className="home-icon4">
                 <path d="M1088 901.166c0 45.5 26.028 84.908 64 104.184v15.938c-10.626 1.454-21.472 2.224-32.5 2.224-68.008 0-129.348-28.528-172.722-74.264-26.222 6.982-54.002 10.752-82.778 10.752-159.058 0-288-114.616-288-256s128.942-256 288-256c159.058 0 288 114.616 288 256 0 55.348-19.764 106.592-53.356 148.466-6.824 14.824-10.644 31.312-10.644 48.7zM512 0c278.458 0 504.992 180.614 511.836 405.52-49.182-21.92-103.586-33.52-159.836-33.52-95.56 0-185.816 33.446-254.138 94.178-70.846 62.972-109.862 147.434-109.862 237.822 0 44.672 9.544 87.888 27.736 127.788-5.228 0.126-10.468 0.212-15.736 0.212-27.156 0-53.81-1.734-79.824-5.044-109.978 109.978-241.25 129.7-368.176 132.596v-26.916c68.536-33.578 128-94.74 128-164.636 0-9.754-0.758-19.33-2.164-28.696-115.796-76.264-189.836-192.754-189.836-323.304 0-229.75 229.23-416 512-416z"></path>
               </svg>
-              <h1 className="home-text38 HeadingOne">Talk and Meet!</h1>
-              <span className="home-text39">
+              <h1 className="home-text34 HeadingOne">Talk and Meet!</h1>
+              <span className="home-text35">
                 <span>Website visitors</span>
                 <span></span>
               </span>
